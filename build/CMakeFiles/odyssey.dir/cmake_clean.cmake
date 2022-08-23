baeabd8342a2c933cf68926c4fbabac43b695c40
Harry Zhou <harry.zhou.xj@outlook.com>
2022-08-22 17:13:30 -0700

    init CMake

---
+++ build/CMakeFiles/odyssey.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odyssey.dir/src/main.cpp.o"
+  "CMakeFiles/odyssey.dir/src/main.cpp.o.d"
+  "odyssey"
+  "odyssey.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odyssey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/odyssey.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for odyssey.
+# This may be replaced when dependencies are built.
