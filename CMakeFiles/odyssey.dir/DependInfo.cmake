b42e0fdbba7772d18119f6acfa13bb8bb8728c67
Harry Zhou <harry.zhou.xj@outlook.com>
2022-08-23 17:33:50 -0700

    build src and update main function to calculate perft value of a given position

---
+++ CMakeFiles/odyssey.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/harry/CS/Odyssey/src/attackers.cpp" "CMakeFiles/odyssey.dir/src/attackers.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/attackers.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/check_evasions.cpp" "CMakeFiles/odyssey.dir/src/check_evasions.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/check_evasions.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/checkers.cpp" "CMakeFiles/odyssey.dir/src/checkers.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/checkers.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/count_moves.cpp" "CMakeFiles/odyssey.dir/src/count_moves.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/count_moves.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/get_fen.cpp" "CMakeFiles/odyssey.dir/src/get_fen.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/get_fen.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/is_legal.cpp" "CMakeFiles/odyssey.dir/src/is_legal.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/is_legal.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/king_allowed.cpp" "CMakeFiles/odyssey.dir/src/king_allowed.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/king_allowed.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/legal_captures.cpp" "CMakeFiles/odyssey.dir/src/legal_captures.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/legal_captures.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/legal_moves.cpp" "CMakeFiles/odyssey.dir/src/legal_moves.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/legal_moves.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/legal_noncaptures.cpp" "CMakeFiles/odyssey.dir/src/legal_noncaptures.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/legal_noncaptures.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/main.cpp" "CMakeFiles/odyssey.dir/src/main.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/main.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/makemove.cpp" "CMakeFiles/odyssey.dir/src/makemove.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/makemove.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/movegen.cpp" "CMakeFiles/odyssey.dir/src/movegen.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/movegen.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/perft.cpp" "CMakeFiles/odyssey.dir/src/perft.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/perft.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/pinned.cpp" "CMakeFiles/odyssey.dir/src/pinned.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/pinned.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/predict_hash.cpp" "CMakeFiles/odyssey.dir/src/predict_hash.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/predict_hash.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/set_fen.cpp" "CMakeFiles/odyssey.dir/src/set_fen.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/set_fen.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/square_attacked.cpp" "CMakeFiles/odyssey.dir/src/square_attacked.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/square_attacked.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/undomove.cpp" "CMakeFiles/odyssey.dir/src/undomove.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/undomove.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/valid.cpp" "CMakeFiles/odyssey.dir/src/valid.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/valid.cpp.o.d"
+  "/home/harry/CS/Odyssey/src/zobrist.cpp" "CMakeFiles/odyssey.dir/src/zobrist.cpp.o" "gcc" "CMakeFiles/odyssey.dir/src/zobrist.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,59 @@
+/* 
+ * The below program initializes a position using fen
+ * and calculate the perft value of the current position
+*/
+
+#include <chrono>
 #include <iostream>
 #include <libchess/position.hpp>
-#include <string>
 
 int main(int argc, char **argv) {
-    /* 
-     * argc counts the number of command-line arguments automatically
-     * argv is the array of strings that contains the arguments
-     * the program below initializes a start position from fen
-     * */
     int depth = 6;
     std::string fen;
-    
+
+    // argc is the number of arguments that the main function takes
+    // argc is automatically counted
+    // argv is a string array that contains all the command-line arguments
+
     if (argc > 1) {
-        depth = std::stoi(argv[1]);
+        depth = std::stoi(std::string(argv[1]));
         depth = std::max(depth, 1);
     }
 
     if (argc > 2) {
-        for (int i = 2; i < argc; i++) {
-            if (fen.empty())
+        for (int i = 2; i < argc; ++i) {
+            if (fen.empty()) {
                 fen = std::string(argv[i]);
-            else
+            } else {
                 fen += " " + std::string(argv[i]);
+            }
         }
     } else {
         fen = "startpos";
     }
 
-    /*
-     * This is how to initialize a position
-     * */
+    // DONE: fix the build issue where I can't initialize a position using a fen
+    // libchess::Position pos = libchess::Position("startpos");
     auto pos = libchess::Position(fen);
+
+    std::cout << pos << std::endl;
+    std::cout << std::endl;
+
+    for (int i = 0; i <= depth; ++i) {
+        const auto t0 = std::chrono::high_resolution_clock::now();
+        const auto nodes = pos.perft(i);
+        const auto t1 = std::chrono::high_resolution_clock::now();
+        const auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+
+        std::cout << "depth " << i;
+        std::cout << " time " << dt.count();
+        std::cout << " nodes " << nodes;
+        if (dt.count() > 0) {
+            const std::uint64_t nps = nodes / dt.count() * 1000;
+            std::cout << " nps " << nps;
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
