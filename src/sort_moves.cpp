9159840e2350f23a445d15b3f2b0fb15d5ad55d9
Harry Zhou <harry.zhou.xj@outlook.com>
2022-10-12 17:56:22 -0700

    enable pv_scoring

--- src/sort_moves.cpp
+++ src/sort_moves.cpp
@@ -1,6 +1,22 @@
 #include <libchess/position.hpp>
 using namespace libchess;
 
+void Position::score_move(Move move) {
+
+}
+
+void Position::enable_pv_scoring(std::vector<Move> move_list) {
+    follow_pv = 0;
+
+    int ply = halfmoves();
+    for (auto move : move_list) {
+        if (pv_table[0][ply] == move) {
+            score_pv = 1;
+            follow_pv = 1;
+        }
+    }
+}
+
 void Position::sort_moves(std::vector<Move> move_list) {
     
 }
